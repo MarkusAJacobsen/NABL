59c0cbf12e24dd71fdf14d1e851c8cea1f1fce00
a7med-95 <ahmed.madhun@hotmail.com>
2018-04-30 17:26:27 +0200

    Feature/contact fire base (#18)
    * Created the Basic GUI for Time Logging
    
    * Everything related to GUI 
    
    the remaing part is to communicate with FireBase
    
    * Fixed Layout
    
    * Fixed for PR #2
    
    * PR Clean code - bug fixed
    
    * Change in a variable name
    
    * Getting data from FireBase and apply it on ExportGUI
    
    * Export GUI finished connected to FireBase
    
    only need exporting to excel
    
    * Constraints and wrong R.id in Project list view

--- app/src/main/java/com/ntnu/wip/nabl/MVCView/ExportView/IExportView.java
+++ app/src/main/java/com/ntnu/wip/nabl/MVCView/ExportView/IExportView.java
@@ -1,27 +1,31 @@
 package com.ntnu.wip.nabl.MVCView.ExportView;
 
 import android.app.DatePickerDialog;
+import android.widget.Adapter;
 
 import com.ntnu.wip.nabl.MVCView.IAbstractMvcView;
 
 import java.util.Date;
 
 /**
+ * Interface to connect between the ExportView (View) and it's Controller (ExportController)
  * Created by a7med on 25.04.18.
  */
 public interface IExportView extends IAbstractMvcView {
+    // Functions to implement in the Controller
     interface ExportInputListener {
         void dateFieldPressed();
         void invalidDateSupplied();
         void exportBtnPressed();
         void changeSelectionBtnPressed();
+        void updateChosenObject(int pos, boolean mode);
     }
 
-    void registerExportListner(ExportInputListener listener);
+    // Functions to implement in the View
+    void registerExportListener(ExportInputListener listener);
     void setDateDialog(DatePickerDialog dialog);
-    void switchView();
+    boolean switchView();
     Date getStart();
     Date getEnd();
-    String getProjectID();
-    String getClientID();
+    void setResourceViewerAdapter(Adapter adapter);
 }
