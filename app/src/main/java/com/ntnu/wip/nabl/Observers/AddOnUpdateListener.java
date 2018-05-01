7dbebaa70d82ab89b8e7c93a77d69d2b0b3f7ca9
MarkusJacobsen <markusja@stud.ntnu.no>
2018-05-01 16:15:54 +0200

    Observer javadoc

--- app/src/main/java/com/ntnu/wip/nabl/Observers/AddOnUpdateListener.java
+++ app/src/main/java/com/ntnu/wip/nabl/Observers/AddOnUpdateListener.java
@@ -2,6 +2,15 @@ package com.ntnu.wip.nabl.Observers;
 
 import java.util.List;
 
+/**
+ * Observer callback
+ * @param <T> Any
+ */
 public interface AddOnUpdateListener<T> {
+
+    /**
+     * On update
+     * @param obj T
+     */
     void onUpdate(T obj);
 }
