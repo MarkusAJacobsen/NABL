403d77777b31fc59d22f87364420d8dd8b5baf69
MarkusJacobsen <markusja@stud.ntnu.no>
2018-05-01 15:17:11 +0200

    Observer test

--- app/src/test/java/com/ntnu/wip/nabl/Observers/ObserverTest.java
+++ app/src/test/java/com/ntnu/wip/nabl/Observers/ObserverTest.java
@@ -1,15 +1,18 @@
 package com.ntnu.wip.nabl.Observers;
 
-import com.ntnu.wip.nabl.Network.AbstractClient;
+import com.ntnu.wip.nabl.Models.Client;
 import com.ntnu.wip.nabl.Network.FirestoreImpl.FireStoreClient;
+import com.ntnu.wip.nabl.Observers.Observers.ObserverFactory;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import static org.junit.Assert.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ObserverTest {
 
@@ -17,10 +20,24 @@ public class ObserverTest {
     public MockitoRule rule = MockitoJUnit.rule();
 
     @Mock
-    private FireStoreClient client;
+    private FireStoreClient network;
 
     @Test
-    public void observerCallBack(){
-        client.getAllProjects();
+    public void observerCallBack() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Client expectedClient = new Client();
+        final Client[] actualClient = new Client[1];
+
+        Method setClients = network.getClass().getDeclaredMethod("setLastFetchedClient", Client.class);
+        setClients.setAccessible(true);
+
+        Observer observer = ObserverFactory.create(ObserverFactory.CLIENT);
+        observer.setSubject(network);
+        observer.setOnUpdateListener(client -> {
+            actualClient[0] = (Client) client;
+        });
+
+        setClients.invoke(network, expectedClient);
+
+        Assert.assertEquals(expectedClient, actualClient[0]);
     }
 }
